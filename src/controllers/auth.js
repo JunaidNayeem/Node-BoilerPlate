022dc429e3a97a39c9322b9e91cec5c57cdfdbb8
JunaidNayeem <junaid85210@gmail.com>
2025-07-01 00:52:15 +0530

    Added vercel file for deploymet-3

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,7 @@
 import httpStatus from 'http-status-codes';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user.js';
 
 const errorResponse = (res, error) => {
   console.error('Error in auth controller:', error);
